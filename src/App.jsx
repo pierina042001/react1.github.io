6381d073a3516f5200f007bfff17494542eb64cf
pierina042001 <137327954+pierina042001@users.noreply.github.com>
2023-10-25 15:46:41 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+import  './components/header/navbar';
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <navbar />
+  );
+}
+
+export default App
---
+++ src/components/header/navbar.jsx
@@ -0,0 +1,19 @@
+const navbar = () => {
+    return (
+        <header>
+            <div className="containerBrand">
+
+            </div>
+
+            <nav className="containerItemList">
+
+            </nav>
+
+            <div className="containerCart">
+
+            </div>
+        </header>
+    )
+}
+
+export default navbar;

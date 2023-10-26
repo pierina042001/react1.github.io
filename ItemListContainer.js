85af36d2a4fab134c48ff46dae4176fede3f0a67
pierina042001 <137327954+pierina042001@users.noreply.github.com>
2023-10-25 22:59:17 -0300

    Add files via upload

---
+++ ItemListContainer.js
@@ -0,0 +1,13 @@
+    import React from 'react';
+
+    function ItemListContainer() {
+    return (
+        <div className="item-list-container">
+        {/* Agrega aquí el contenido de tu lista de productos */}
+        <h2>Productos Disponibles</h2>
+        {/* Por ejemplo, puedes mostrar una lista de productos aquí */}
+        </div>
+    );
+    }
+
+    export default ItemListContainer;
